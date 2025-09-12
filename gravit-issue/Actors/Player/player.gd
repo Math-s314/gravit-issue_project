f82b07adefe70382d5476601d0fbe030d0afad3f
Math's <math_s@outlook.fr>
2025-09-13 01:15:20 +0200

    Smoothing player animations

--- gravit-issue/Actors/Player/player.gd
+++ gravit-issue/Actors/Player/player.gd
@@ -13,7 +13,7 @@ class_name Player
 @export var air_control : float
 
 ## Gravity switching
-var gravity_switch := 1.0;
+var gravity_dir := 1.0; # UP is 1, DOWN is -1
 var gravity_transition := false
 @onready var gravity_timer := $GravityTimer
 @onready var sprite := $AnimatedSprite2D
@@ -22,82 +22,110 @@ var gravity_transition := false
 ## Display informations
 const EPSILON := 1e-2
 const BASE_EMITTER := Transform2D(0.0, Vector2(0.0, 27.0))
-const WALKING_EMITTER := Transform2D(deg_to_rad(47.0), Vector2(-10.0, 24.3))
+const WALKING_EMITTER := Transform2D(deg_to_rad(35.0), Vector2(-8.0, 24.5))
+const MID_DOWN_EMITTER := Transform2D(0.0, Vector2(0.0, 21.5))
+const MID_UP_EMITTER := Transform2D(deg_to_rad(180.0), Vector2(0.0, -21.5))
 
 var min_particle_speed : float
 var max_particle_speed : float
 
+## Input information
+var inputAxis := 0.0
+var start_jump := false
+var start_reverse := false
+
+
 func _ready() -> void:
-	gravity_timer.wait_time = still_duration
+	gravity_timer.start(still_duration)
 	min_particle_speed = particles.initial_velocity_min
 	max_particle_speed = particles.initial_velocity_max
+	
+	sprite.play(&"Idle") # To avoid blocking animations...
 
 func _process(delta: float) -> void:
 	# Gravity
+	print(is_on_floor())
 	if(!is_on_floor()): velocity.y += get_gravity_coef() * gravity_strength * delta;
 	else: velocity.y = 0.0
 	
-	# Player input
-	var inputAxis :=  Input.get_axis("Left", "Right") * (1.0 if is_on_floor() else air_control)
-	particles.initial_velocity_min = lerp(min_particle_speed, 1.5 * min_particle_speed, abs(inputAxis))
-	particles.initial_velocity_max = lerp(max_particle_speed, 1.5 * max_particle_speed, abs(inputAxis))
-	velocity = Vector2(inputAxis * move_speed, velocity.y)
-	
-	if abs(inputAxis) > EPSILON and sprite.animation != &"Demi-tour" and sprite.animation != &"Renversement":
-		if inputAxis > 0.0 and sprite.flip_h == true :
-			sprite.play(&"Demi-tour")
-			particles.transform = BASE_EMITTER
-		elif inputAxis  < 0.0 and sprite.flip_h == false : 
-			sprite.play(&"Demi-tour")
-			particles.transform = BASE_EMITTER
-		else : 
-			sprite.play("Walking")
-			particles.transform = WALKING_EMITTER
-			particles.position.x *= -1.0 if sprite.flip_h else 1.0
-			particles.rotation *= -1.0 if sprite.flip_h else 1.0
-	elif abs(inputAxis) < EPSILON and sprite.animation != &"Renversement" :
-		sprite.play("Idle")
-		particles.transform = BASE_EMITTER
-	
-	
-	
-
-	if Input.is_action_just_pressed("Jump") && is_on_floor():
-		velocity.y = -gravity_switch * jump_force
-
+	handle_input()
+	handle_animation()
+	handle_particle()
 	move_and_slide()
-	
-	if Input.is_action_just_released("Gravité") :
-		sprite.play("Renversement")
-		_on_gravity_switch()
-	
 
 func get_gravity_coef() -> float:
 		if(!gravity_transition):
-			return gravity_switch
+			return gravity_dir
 		else:
 			var progress : float = gravity_timer.time_left/transition_duration;
-			return gravity_switch * cos(PI * progress)
+			return gravity_dir * cos(PI * progress)
+
+func handle_input() -> float:
+	inputAxis =  Input.get_axis("Left", "Right") * (1.0 if is_on_floor() else air_control)
+
+	if Input.is_action_just_pressed("Jump") && is_on_floor():
+		velocity.y = -gravity_dir * jump_force
+	velocity = Vector2(inputAxis * move_speed, velocity.y)
+
+	if Input.is_action_just_released("Gravité") : _on_gravity_switch()
 	
+	return inputAxis
+	
+func handle_animation(ended := &"") -> void:
+	# Blocking animations
+	if sprite.animation == &"Demi-tour" and ended != &"Demi-tour" : return
+	if sprite.animation == &"Renversement" and ended != &"Renversement" : return
+	
+	# Looping animations
+	if abs(inputAxis) > EPSILON :
+		if inputAxis > 0.0 and sprite.flip_h == true : sprite.play(&"Demi-tour")
+		elif inputAxis  < 0.0 and sprite.flip_h == false : sprite.play(&"Demi-tour")
+		else : sprite.play(&"Walking")
+	elif abs(inputAxis) < EPSILON :
+		sprite.play(&"Idle")
+		particles.transform = BASE_EMITTER
+
+func handle_particle() -> void:
+	var kill := false
+	
+	match sprite.animation:
+		&"Demi-tour": particles.transform = BASE_EMITTER
+		&"Walking":
+			particles.transform = WALKING_EMITTER
+			particles.position.x *= -1.0 if sprite.flip_h else 1.0
+			particles.rotation *= -1.0 if sprite.flip_h else 1.0
+		&"Renversement":
+			match sprite.frame:
+				0: particles.transform = MID_DOWN_EMITTER
+				1: kill = true
+				2: particles.transform = MID_UP_EMITTER
+		_: particles.transform = BASE_EMITTER
+	
+	particles.visible = !kill
+		
 func _on_gravity_switch():
-	if(gravity_transition): # Starting still period
-		scale.y = abs(scale.y)*gravity_switch
+	if(gravity_transition): # Ending transition period
 		gravity_transition = false
 		gravity_timer.wait_time = still_duration
 	else: # Starting transition period
 		gravity_transition = true
 		gravity_timer.wait_time = transition_duration
-		gravity_switch = -gravity_switch
+		gravity_dir = -gravity_dir
+		sprite.play(&"Renversement")
 	gravity_timer.start();
 
 func _on_animation_looped() -> void:
-	if sprite.animation == "Demi-tour" :
+	if sprite.animation == &"Demi-tour" :
 		sprite.flip_h = not sprite.flip_h
 		sprite.stop()
-		sprite.play("Walking") # Replace with function body.
-	if sprite.animation == "Renversement" :
+		
+		handle_animation(&"Demi-tour")
+		handle_particle()
+		
+	if sprite.animation == &"Renversement" :
+		scale.y = gravity_dir * abs(scale.y)
+		up_direction.y = -gravity_dir
 		sprite.stop()
-		print("fkseiopf")
-		scale.y = gravity_switch
-		up_direction.y = -up_direction.y
-		sprite.play("Idle")
+
+		handle_animation(&"Renversement")
+		handle_particle()
