bc043a687e11420c2d1ee493eaff2d782dd02adb
Azerthias <matthias.hayart@gmail.com>
2025-09-26 17:37:51 +0200

    magnet + level 4 and 6

--- gravit-issue/Actors/Magnet/Magnet.gd
+++ gravit-issue/Actors/Magnet/Magnet.gd
@@ -15,16 +15,18 @@ func _on_stop_attraction() -> void:
 	GameInstance.getLevelManager().player.kill_input = false
 	
 func _process(delta: float) -> void:
-	if Input.is_action_just_released("Magnet") && detection_length > (position - GameInstance.getLevelManager().player.position).length():
+	if Input.is_action_just_pressed("Magnet") && detection_length > (position - GameInstance.getLevelManager().player.position).length():
 		if active : _on_stop_attraction()
 		else : _on_start_attraction()
 		
 	if active:
-		var player := GameInstance.getLevelManager().player		
+		var player := GameInstance.getLevelManager().player
 		player.velocity += (position - player.position) * strength * delta
 		player.move_and_slide()
 		
 		if (player.position - position).length() < release_length:
+			_on_stop_attraction()
+		elif player.is_on_floor() or player.is_on_ceiling():
 			_on_stop_attraction()
 		
 		
