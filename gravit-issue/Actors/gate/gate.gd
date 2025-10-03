a0cb3a5cb09da6b033cf21593a5bf6c5f17df17a
Azerthias <matthias.hayart@gmail.com>
2025-10-03 16:39:31 +0200

    gate and button

---
+++ gravit-issue/Actors/gate/gate.gd
@@ -0,0 +1,11 @@
+extends StaticBody2D
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+var is_open: bool = false
+	
+func change_state() -> void:
+	is_open = not is_open
+	if(is_open):
+		animation_player.play("animation")
+	else:
+		animation_player.play_backwards("animation")
